e4623613e6909c8e1bccc1f5bb55ce89bb63e175
Maxwell Wehner <maxwehnerwork@gmail.com>
2021-07-15 17:24:57 -0700

    clean up testing routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,9 @@
 const express = require("express");
-const router = express.Router();
+
 const apiRouter = require("./api");
 
-router.use("/api", apiRouter);
+const router = express.Router();
 
-router.get("/hello/world", (req, res) => {
-  res.cookie("XSRF-TOKEN", req.csrfToken());
-  res.send("Hello World");
-});
+router.use("/api", apiRouter);
 
 module.exports = router;
