cc689e3eb61694d6df9a57c8aec9a748c8f87f29
Maxwell Wehner <maxwehnerwork@gmail.com>
2021-07-16 10:59:47 -0700

    set up csrf to be used on the frontend

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,11 +6,11 @@ router.use("/session", sessionRouter);
 
 router.use("/users", usersRouter);
 
-router.post("/test", (req, res) => {
-  res.json({
-    requestBody: req.body,
-  });
-});
+// router.post("/test", (req, res) => {
+//   res.json({
+//     requestBody: req.body,
+//   });
+// });
 
 module.exports = router;
 
