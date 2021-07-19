73fe56145e96909a4faa88b9cb23eddef2c2c705
Maxwell Wehner <maxwehnerwork@gmail.com>
2021-07-19 01:11:27 -0700

    added logout

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -60,6 +60,15 @@ export const signup = (user) => async (dispatch) => {
   return response;
 };
 
+//logout thunk
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session", {
+    method: "DELETE",
+  });
+  dispatch(removeUser());
+  return response;
+};
+
 //reducer
 const sessionReducer = (state = initialState, action) => {
   let newState;
