8e1bf55efc02d55eacec3028c6807976d7fb20ed
Maxwell Wehner <maxwehnerwork@gmail.com>
2021-07-26 05:02:10 -0700

    fixed spacing on main page

--- frontend/src/components/Spots/index.js
+++ frontend/src/components/Spots/index.js
@@ -29,31 +29,34 @@ const Spots = () => {
   }
 
   return (
-    <div className="spots__container">
-      {spots &&
-        spots.map((spot) => {
-          return (
-            <div className="spot__container" key={spot.id}>
-              <img
-                className="first-image"
-                src={spot.Images[0].url}
-                alt="treehouse property"
-                onClick={() => handleSingleSpot(spot)}
-              />
-              <div className="text-info__container">
-                <h2 className="spot__name">{spot.name}</h2>
-                <div className="address">
-                  Address:
-                  <span> {spot.city}, </span>
-                  <span>{spot.state}, </span>
-                  <span>{spot.country}</span>
+    <>
+      <div className="spots__container">
+        {spots &&
+          spots.map((spot) => {
+            return (
+              <div className="spot__container" key={spot.id}>
+                <img
+                  className="first-image"
+                  src={spot.Images[0].url}
+                  alt="treehouse property"
+                  onClick={() => handleSingleSpot(spot)}
+                />
+                <div className="text-info__container">
+                  <h2 className="spot__name">{spot.name}</h2>
+                  <div className="address">
+                    Address:
+                    <span> {spot.city}, </span>
+                    <span>{spot.state}, </span>
+                    <span>{spot.country}</span>
+                  </div>
+                  <div className="spot__price">Price: ${spot.price}/night</div>
                 </div>
-                <div className="spot__price">Price: ${spot.price}/night</div>
               </div>
-            </div>
-          );
-        })}
-    </div>
+            );
+          })}
+      </div>
+      <div className="spacer"></div>
+    </>
   );
 };
 
